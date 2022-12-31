33def281092697fe65f466157aa1521ff0cb9e42
Vinicius Boscoa <virb30@gmail.com>
2022-12-30 23:29:53 -0300

    CRUD with SQLC

---
+++ 15-sqlc/cmd/runSQLC/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/virb30/curso-go/15-sqlc/internal/db"
+)
+
+func main() {
+	ctx := context.Background()
+	dbConn, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/courses")
+	if err != nil {
+		panic(err)
+	}
+	defer dbConn.Close()
+
+	queries := db.New(dbConn)
+
+	// err = queries.CreateCategory(ctx, db.CreateCategoryParams{
+	// 	ID:          uuid.New().String(),
+	// 	Name:        "Backend",
+	// 	Description: sql.NullString{String: "Backend description", Valid: true},
+	// })
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// categories, err := queries.ListCategories(ctx)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// for _, category := range categories {
+	// 	println(category.ID, category.Name, category.Description.String)
+	// }
+
+	// err = queries.UpdateCategory(ctx, db.UpdateCategoryParams{
+	// 	ID:          "8ed0cc9e-0513-4b21-b680-b912d3d7a4a9",
+	// 	Name:        "Backend updated",
+	// 	Description: sql.NullString{String: "Backend description updated", Valid: true},
+	// })
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// categories, err := queries.ListCategories(ctx)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// for _, category := range categories {
+	// 	println(category.ID, category.Name, category.Description.String)
+	// }
+
+	err = queries.DeleteCategory(ctx, "ab131598-e844-489b-ac86-f4f5675445fc")
+
+	if err != nil {
+		panic(err)
+	}
+
+	categories, err := queries.ListCategories(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, category := range categories {
+		println(category.ID, category.Name, category.Description.String)
+	}
+}
---
+++ 15-sqlc/go.mod
@@ -0,0 +1,8 @@
+module github.com/virb30/curso-go/15-sqlc
+
+go 1.19
+
+require (
+	github.com/go-sql-driver/mysql v1.7.0
+	github.com/google/uuid v1.3.0
+)
