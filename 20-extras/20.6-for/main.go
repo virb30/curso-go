48da587ee64f9f4e496bb32239618fb609aafabc
Vinicius Boscoa <virb30@gmail.com>
2024-05-13 22:55:10 -0300

    go extras

--- 20-extras/20.6-for/main.go
+++ 20-extras/20.6-for/main.go
@@ -3,8 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	done := make(chan bool)
+	for i := range 10 {
+		fmt.Println(i)
+	}
 
+	done := make(chan bool)
 	values := []string{"a", "b", "c"}
 	for _, v := range values {
 		go func() {
